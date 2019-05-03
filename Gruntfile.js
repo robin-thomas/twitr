efce52611007334cb63b21a89ff8dc12c54908c2
Robin Thomas <robinthomas2591@gmail.com>
2019-05-03 11:27:47 +0800

    npm + grunt + eslint

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = (grunt) => {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    eslint: {
+      target: [
+        './src/js/*.js'
+      ],
+      options: {
+        configFile: './eslint.json',
+        globals: ['$'],
+      },
+    },
+    browserify: {
+      target: {
+        src: [ './src/js/*.js' ],
+        dest: './src/main.js',
+      },
+    },
+    uglify: {
+      target: {
+        src: './src/main.js',
+        dest: './src/main.min.js'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-eslint');
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-uglify-es');
+  grunt.registerTask('default', [
+    'eslint',
+    'browserify',
+    // 'uglify',
+  ]);
+};
