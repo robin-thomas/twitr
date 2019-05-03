4376f1a45011f3114cb54af6b92d33b050c83680
Robin Thomas <robinthomas2591@gmail.com>
2019-05-03 11:17:25 +0800

    tweet image preview

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ $(document).ready(() => {
   const tweetSubmit = $('#confirm-tweet');
   const tweetAddPic = $('#tweet-img-fake');
   const tweetAddPicReal = $('#tweet-img');
+  const tweetImgShow = $('#tweet-img-show');
+  const tweetImgDelete = $('#tweet-img-delete');
 
   // Show the tweet input window.
   tweetBtn.on('click', () => {
@@ -43,18 +45,44 @@ $(document).ready(() => {
   });
 
   tweetAddPic.on('click', () => tweetAddPicReal.click());
+  tweetAddPicReal.on('change', function(e) {
+    const file = e.target.files[0];
+    console.log(file);
+
+    if (file.size > (1024 * 1024)) {
+      $(this).val('');
+      return;
+    }
+
+    const reader = new FileReader();
+    reader.onload = function(){
+      const dataURL = reader.result;
+      tweetImgShow.attr('src', dataURL);
+      tweetImgShow.fadeIn();
+      tweetImgDelete.fadeIn();
+    };
+    reader.readAsDataURL(file);
+
+    $(this).val('');
+  });
+
+  tweetImgDelete.on('click', () => {
+    tweetImgDelete.fadeOut();
+    tweetImgShow.fadeOut();
+    tweetImgShow.attr('src', '');
+  });
 
   // NEAR initialization code.
-  const init = async () => {
-    try {
-      config = await nearlib.dev.getConfig();
-      contractId = config.contractName;
-      baseUrl = config.appUrl;
-    } catch (err) {
-      throw err;
-    }
-  };
-
-  init().catch(console.log);
+  // const init = async () => {
+  //   try {
+  //     config = await nearlib.dev.getConfig();
+  //     contractId = config.contractName;
+  //     baseUrl = config.appUrl;
+  //   } catch (err) {
+  //     throw err;
+  //   }
+  // };
+  //
+  // init().catch(console.log);
 
 });
